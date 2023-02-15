6e383b65361252c665fe824bcd6991c9ef989934
Shuvo <shuvor@student.douglascollege.ca>
2023-02-14 18:02:56 -0800

    week 4 incomplete

--- week4Lab/app/src/main/java/com/example/week4lab/MainActivity.java
+++ week4Lab/app/src/main/java/com/example/week4lab/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.week4lab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +26,23 @@ public class MainActivity extends AppCompatActivity {
         final EditText weight = (EditText) findViewById(R.id.patientWght);
         final RadioButton lbToKg = (RadioButton) findViewById(R.id.radPdToKilo);
         final RadioButton kgTolb = (RadioButton) findViewById(R.id.radKiloToPd);
+        final TextView result = (TextView) findViewById(R.id.results);
+        Button convert = (Button) findViewById(R.id.convertBtn);
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                weightEntered = Double.parseDouble(weight.getText().toString());
+                DecimalFormat tenth = new DecimalFormat("#.#");
+                if(lbToKg.isChecked()){
+                    if(weightEntered <= 500){
+                        convertedWeight = weightEntered / conversionRate;
+                        result.setText(tenth.format(convertedWeight) + " KG");
+                    }
+                } else {
+
+                }
+            }
+        });
+
     }
 }
